6535b3cdb66e3b222a32dddd888b71bb4ebc243b
Peidong He <38892121+Thomasbulls@users.noreply.github.com>
2022-08-25 13:43:21 -0400

    modified comments

--- deep-copy/deep-clone.js
+++ deep-copy/deep-clone.js
@@ -5,20 +5,27 @@
  */
 
   const deepClone = (input) => {
+      //make sure the input is an object or array
   if ( input == null || typeof input !== "object") {
       return input;    //primitive
   }
 
-  //initialization 初始化返回值
+  //initialization 初始化返回值 for recursion
+    //determine the type of the input
   const initialOutput = Array.isArray(input) ? [] : {};
+  //recursion
   return Object.keys(input).reduce((acc, key) => {
+      //similar to for (let key in input)
       acc[key] = deepClone(input[key])
       return acc;
   }, initialOutput);
-
 }
 
 module.exports = deepClone
+
+
+
+
 
 //simple test for objects
 let obj1 = {
